e1c780c50c7078f2f4c2622e69db056bf3b2d2d0
Mohammad Rezai <hussy.rez@gmail.com>
2020-09-08 20:39:42 +1000

    Added bootstrap cdn and its dependencies

--- app.js
+++ app.js
@@ -24,6 +24,19 @@ app.use(bodyParser.json())
 var routes = require('./routes/index');
 app.use('/', routes);
 
+app.use(favicon(__dirname + '/public/images/fav_icon.png'));
+app.use(logger('dev'));
+
+ /**
+ *  App Configuration
+ */
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+// Telling the app to use the public folder to serve our files is a nice and clean way to manage our site assets
+app.use(express.static(path.join(__dirname, "public")));
+
 // catch 404 and forward to error handler
 // this matches all routes and all methods
 app.use(function(req, res, next) {
@@ -36,23 +49,11 @@ app.use(function(req, res, next) {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) { 
   res.status(err.status || 500);
-  console.log(res.locals.error)
   res.render('error', {
     message: err.message,
     error: (app.get('env') === 'development' ? err : {}) // no stacktraces leaked to user
   });
 });
-
- /**
- *  App Configuration
- */
-
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-
-// Telling the app to use the public folder to serve our files is a nice and clean way to manage our site assets
-app.use(express.static(path.join(__dirname, 'public')));
-
 /**
  * Routes Definitions
  */
